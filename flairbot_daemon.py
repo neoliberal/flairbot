e2bcc645a30f6a74ebd3bb70798098fab58cfa59
Abhi Agarwal <goldcobrapro@gmail.com>
2017-11-26 22:56:57 -0500

    Add variables for DaemonContext

--- flairbot_daemon.py
+++ flairbot_daemon.py
@@ -1,5 +1,6 @@
 """daemon runner"""
 import daemon
+from daemon import pidfile
 
 from flairbot import Flairbot
 
@@ -11,16 +12,22 @@ def main() -> None:
     reddit: praw.Reddit = praw.Reddit(
         client_id=os.environ["client_id"],
         client_secret=os.environ["client_secret"],
-        refresh_token=os.environ["request_token"]
+        refresh_token=os.environ["request_token"],
+        user_agent="linux:flairbot:v3.0 (by /u/CactusChocolate)"
     )
     bot: Flairbot = Flairbot(
         reddit,
         "neoliberal",
-        os.environ["url"]
+        os.environ["slack_webhook_url"]
     )
-    with daemon.DaemonContext():
-        bot.fetch_pms()
-    return
+    with daemon.DaemonContext(
+        working_directory="/var/lib/flairbot",
+        umask=0o002,
+        pidfile=pidfile.TimeoutPIDLockFile(
+            "/var/run/flairbot.pid")
+        ):
+        while True:
+            bot.fetch_pms()
 
 if __name__ == "__main__":
     main()
