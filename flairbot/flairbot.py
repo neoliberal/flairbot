9b2d1d3c128c4f57b4a77a799336e7aa55331ddb
Abhi Agarwal <goldenercobra@gmail.com>
2017-12-10 18:40:16 -0500

    Spooky scary skeleton

--- flairbot/flairbot.py
+++ flairbot/flairbot.py
@@ -1,8 +1,15 @@
 """flairbot"""
+import logging
+
+from slack_python_logging import slack_logger
 
 from changer.changer import FlairChanger
 from page.updater import WikiUpdater
 from stats.updater import StatisticsUpdater
 
 class Flairbot(object):
-    pass
+    def __init__() -> None:
+        logger: logging.Logger = slack_logger.initialize("flairbot")
+        return
+
+    def run() -> None:
