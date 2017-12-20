0a2b3771b302255e436800ea4e1ca48f63ebb197
Abhi Agarwal <goldenercobra@gmail.com>
2017-12-20 18:05:04 -0500

    Update to new system

--- flairbot.py
+++ flairbot.py
@@ -1,7 +1,7 @@
 """Set user flairs."""
 from configparser import ConfigParser
 import logging
-from typing import Optional, Tuple, FrozenSet, List
+from typing import Optional, Tuple, List
 
 import praw
 from slack_python_logging import slack_logger
@@ -11,45 +11,31 @@ class Flairbot(object):
 
     def __init__(self, reddit: praw.Reddit, subreddit: str) -> None:
         """Initial setup"""
-        def get_config() -> ConfigParser:
-            """returns config"""
-            self.logger.debug("Creating config")
-            parser: ConfigParser = ConfigParser(allow_no_value=True)
-
-            self.logger.debug("Getting config")
-            config_str: str = self.subreddit.wiki["flairbot/config"].content_md
-            if config_str:
-                self.logger.debug("Got config")
-                parser.read_string(config_str)
-            else:
-                self.logger.error("No config found")
-
-            self.logger.debug("config created")
-            return parser
-
         self.logger: logging.Logger = slack_logger.initialize("flairbot")
         self.reddit: praw.Reddit = reddit
         self.subreddit: praw.models.Subreddit = self.reddit.subreddit(subreddit)
-        self.config = get_config()
-        self.text_flairs: FrozenSet[str] = frozenset(
-            self.config["types"]
-        )
-        self.flairs = self.get_flairs()
+        self.config: ConfigParser = self.get_config()
+        self.image_flairs: ConfigParser = self.get_config("flair")
+        self.text_flairs: ConfigParser = self.get_config("text")
         self.logger.info("Initalized successfully")
+        return
 
-    def get_flairs(self) -> ConfigParser:
-        """fetches flairs"""
-        self.logger.debug("Fetching flairs")
-        flairs: ConfigParser = ConfigParser()
-        flairs_str: str = self.subreddit.wiki["flairbot/config/flairs"].content_md
-        if flairs_str:
-            self.logger.debug("Fetched flairs")
-            flairs.read_string(flairs_str)
+    def get_config(self, section: Optional[str] = None) -> ConfigParser:
+        """returns config"""
+        self.logger.debug("Creating config")
+        parser: ConfigParser = ConfigParser(allow_no_value=True)
+
+        self.logger.debug("Getting config")
+        config_location = '/'.join(item for item in ["flairbot", "config", section] if item)
+        config_str: str = self.subreddit.wiki[config_location].content_md
+        if config_str:
+            self.logger.debug("Got config \"%s\"", config_location)
+            parser.read_string(config_str)
         else:
-            self.logger.critical("No flairs found. Aborting.")
-            import sys
-            sys.exit(0)
-        return flairs
+            self.logger.error("Config \"%s\" found", config_location)
+
+        self.logger.debug("Config created")
+        return parser
 
     def fetch_pms(self) -> None:
         """Get PMs for account"""
@@ -69,18 +55,18 @@ class Flairbot(object):
                     )
                     self.process_pm(message)
         except prawcore.exceptions.RequestException:
-            self.logger.exception("Request error: Sleeping for 60 seconds.")
+            self.logger.exception("Request error: Sleeping for 5 minutes.")
             import time
-            time.sleep(60)
+            time.sleep(60 * 5)
 
     def process_pm(self, message: praw.models.Message) -> None:
         """Process the PMs"""
         self.logger.debug("Updating flairs")
-        self.flairs = self.get_flairs()
+        self.image_flairs = self.get_config("flairs")
         self.logger.debug("Updated flairs")
 
         flair: str = message.body
-        result: Optional[Tuple[str, str, str]] = self.get_flair_properties(flair)
+        result: Optional[Tuple[str, str, str]] = self.get_image_flair_properties(flair)
         message.mark_read()
         if result is None:
             self.send_pm_failure(message)
@@ -88,7 +74,7 @@ class Flairbot(object):
 
         self.set_flair(message.author, result[0], result[1], result[2])
 
-    def get_flair_properties(self, flair: str) -> Optional[Tuple[str, str, str]]:
+    def get_image_flair_properties(self, flair: str) -> Optional[Tuple[str, str, str]]:
         """
         Match flair selection to correct category
 
@@ -97,16 +83,16 @@ class Flairbot(object):
         self.logger.debug("Getting flair properties")
         try:
             section: str = next((
-                section for section in self.flairs.sections()
-                if flair in self.flairs.options(section)
+                section for section in self.image_flairs.sections()
+                if flair in self.image_flairs.options(section)
             ))
         except StopIteration:
             self.logger.warning("Flair \"%s\" does not exist", flair)
             return None
 
-        text: str = self.flairs[section][flair]
+        default_text: str = self.image_flairs[section][flair]
         self.logger.debug("Got flair properties")
-        return (section, flair, text)
+        return (section, flair, default_text)
 
     # pylint: disable=R0201
     def send_pm_failure(self, message: praw.models.Message):
@@ -129,22 +115,31 @@ class Flairbot(object):
 
     def set_flair(self, user: praw.models.Redditor, section: str, flair: str, text: str):
         """Set the flairs"""
-
         for current_user_flair in self.subreddit.flair(redditor=user):
-            self.logger.debug("Setting flair for /u/%s", user)
             current_class: Optional[str] = current_user_flair["flair_css_class"]
             decomposed_class: List[str] = [] if current_class is None else current_class.split(' ')
             current_text: str = current_user_flair["flair_text"]
 
+            self.logger.debug("Setting flair for /u/%s with class \"%s\"", user, current_class)
             new_class: List[str] = []
 
             special: bool = False
-            for text_flair in self.text_flairs:
-                if text_flair in decomposed_class:
-                    self.logger.debug("/u/%s is special", user)
+            for role in self.text_flairs.options("roles"):
+                if role in decomposed_class:
                     special = True
-                    new_class.append(text_flair)
-                    break
+                    flair_color: str = self.text_flairs["roles"][role]
+                    new_class.extend([role, flair_color, "text"])
+
+            for special_role in self.text_flairs.options("special_roles"):
+                if special_role in decomposed_class:
+                    special = True
+                    distinguished_class: str = self.text_flairs["special_roles"][special_role]
+                    new_class.extend([special_role, distinguished_class])
+                    special_category: str = f"{special_role}_roles"
+                    for sub_role in self.text_flairs.options(special_category):
+                        if sub_role in decomposed_class:
+                            distinguished_color: str = self.text_flairs[special_category][sub_role]
+                            new_class.extend([sub_role, distinguished_color, "text"])
 
             new_class.extend([section, flair, "image"])
             new_text: str = current_text if special else text
