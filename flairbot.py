9d55a6f885114a0ebfcff5828a2a75c5f13afc38
Abhi Agarwal <goldenercobra@gmail.com>
2018-01-08 16:28:55 -0500

    Use new flair locations

--- flairbot.py
+++ flairbot.py
@@ -15,7 +15,7 @@ class Flairbot(object):
         self.reddit: praw.Reddit = reddit
         self.subreddit: praw.models.Subreddit = self.reddit.subreddit(subreddit)
         self.config: ConfigParser = self.get_config()
-        self.image_flairs: ConfigParser = self.get_config("flairs")
+        self.image_flairs: ConfigParser = self.get_config("images")
         self.text_flairs: ConfigParser = self.get_config("text")
         self.logger.info("Initalized successfully")
         return
@@ -66,7 +66,7 @@ class Flairbot(object):
     def process_pm(self, message: praw.models.Message) -> None:
         """Process the PMs"""
         self.logger.debug("Updating image and text flairs")
-        self.image_flairs = self.get_config("flairs")
+        self.image_flairs = self.get_config("images")
         self.text_flairs = self.get_config("text")
         self.logger.debug("Updated flairs")
 
